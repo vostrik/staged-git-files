d6f26e969541e81b2fd61b1e825f3564bc746a28
Pavel Vostrikov <mail@vostrik.com>
2018-05-16 23:21:47 +0300

    Refactor tests to crossplatform implementation

--- test/test.js
+++ test/test.js
@@ -1,196 +1,211 @@
 require('should');
-var fs = require('fs');
-var del = require('del')
-exec = require('child_process').exec;
-test_folder = process.cwd() + '/test/test-dir';
+var fs = require('fs')
+var path = require('path')
+var fsExtra = require('fs-extra')
+exec = require('child_process').exec
+var testFolderRelative = path.normalize('test/test-dir')
+var testFolder = path.join(process.cwd(), testFolderRelative)
 
-asyncCatch = function(done, test){
-    return function(err, value){
-        if(err){
+module.exports = {
+    asyncCatch: asyncCatch,
+    cleanUp: cleanUp,
+    setup: setup,
+    newSGF: newSGF,
+    run: run,
+    newGit: newGit,
+    randomFileName: randomFileName,
+    randomFileContent: randomFileContent,
+    randomString: randomString,
+    moveFile: moveFile,
+    newFile: newFile,
+    addFile: addFile,
+    addFiles: addFiles,
+    addAndCommitFile: addAndCommitFile,
+    testFolder
+}
+
+function asyncCatch (done, test) {
+    return function (err, value) {
+        if (err) {
             done(err);
         }
-        else{
-            try{
+        else {
+            try {
                 test(value);
                 done();
             }
-            catch(err){
+            catch (err) {
                 done(err);
             }
         }
     }
-  }
 }
 
-cleanUp = function (callback) {
+function cleanUp (callback) {
     try {
-        var paths = del.sync(test_folder)
+        var paths = fsExtra.removeSync(testFolder)
         callback(null, paths)
     } catch (error) {
         callback(error)
     }
 }
 
-setup = function (callback) {
-  cleanUp(function (err) {
-    if (err) {
-      callback(err)
-    } else {
-      exec('mkdir \'' + test_folder + '\'', function (err, stdout, stderr) {
-        if (err || stderr) {
-          callback(err || new Error(stderr))
+function setup (callback) {
+    cleanUp(function (err) {
+        if (err) {
+            callback(err)
         } else {
-          callback(null, stdout)
+            fsExtra.mkdirp(testFolder, function (err, result) {
+                callback(err || null, result)
+            })
         }
-      })
-    }
-  })
+    })
 }
 
-newSGF = function () {
-  delete require.cache[require.resolve('../')]
+function newSGF () {
+    delete require.cache[ require.resolve('../') ]
 
-  var sgf = require('../')
-  sgf.cwd = test_folder
-  return sgf
+    var sgf = require('../')
+    sgf.cwd = testFolder
+    return sgf
 }
 
-run = function (command, callback) {
-  exec('cd \'' + test_folder + '\' && ' + command, callback)
+function run (command, callback) {
+    exec('cd "' + testFolderRelative + '" && git init', callback)
 }
 
-newGit = function (callback) {
+function newGit (callback) {
     try {
-        del.sync('.git')
+        fsExtra.removeSync(path.join(testFolder, '.git'))
         run('git init', callback)
     } catch (error) {
         callback(error)
     }
 }
 
-randomFileName = function (lengths) {
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  var filename = randomString(possible, lengths[0])
-  for (var i = 1; i < lengths.length; i++) {
-    filename += '.' + randomString(possible, lengths[i])
-  }
-  return filename
+function randomFileName (lengths) {
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var filename = randomString(possible, lengths[ 0 ])
+    for (var i = 1; i < lengths.length; i++) {
+        filename += '.' + randomString(possible, lengths[ i ])
+    }
+    return filename
 }
 
-var randomFileContent = function (length) {
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 \t\n,.;\'[](){}":?><'
-  return randomString(possible, length)
+function randomFileContent (length) {
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 \t\n,.;\'[](){}":?><'
+    return randomString(possible, length)
 }
 
-var randomString = function (possible, length) {
-  var text = ''
+function randomString (possible, length) {
+    var text = ''
 
-  while (length--) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length))
-  }
+    while (length--) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
 
-  return text
+    return text
 }
 
-moveFile = function (opts, callback) {
-  var oldPath = test_folder + '/' + opts.oldFileName
-  var newPath = test_folder + '/' + opts.newFileName
+function moveFile (opts, callback) {
+    var oldPath = path.join(testFolder, opts.oldFileName)
+    var newPath = path.join(testFolder, opts.newFileName)
 
-  fs.rename(oldPath, newPath, function (err) {
-    if (err) {
-      callback(err)
-    } else {
-      run('git add ' + opts.oldFileName + ' ' + opts.newFileName, function (err, stdout, stderr) {
-        if (err || stderr) {
-          callback(err || new Error(stderr))
+    fs.rename(oldPath, newPath, function (err) {
+        if (err) {
+            callback(err)
         } else {
-          callback(null, opts)
+            run('git add ' + opts.oldFileName + ' ' + opts.newFileName, function (err, stdout, stderr) {
+                if (err || stderr) {
+                    callback(err || new Error(stderr))
+                } else {
+                    callback(null, opts)
+                }
+            })
         }
-      })
-    }
-  })
+    })
 }
 
-newFile = function (opts, callback) {
+function newFile (opts, callback) {
 
-  if (typeof opts == 'function') {
-    callback = opts
-    opts = {}
-  }
-
-  opts.filename = opts.filename || randomFileName([8, 3])
-  opts.content = opts.content || randomFileContent(10000)
-
-  fs.writeFile(test_folder + '/' + opts.filename, opts.content, function (err) {
-    if (err) {
-      callback(err)
-    } else {
-      callback(null, opts)
+    if (typeof opts == 'function') {
+        callback = opts
+        opts = {}
     }
-  })
-}
 
-addFile = function (opts, callback) {
-  if (typeof opts == 'function') {
-    callback = opts
-    opts = {}
-  }
+    opts.filename = opts.filename || randomFileName([ 8, 3 ])
+    opts.content = opts.content || randomFileContent(10000)
 
-  newFile(opts, function (err, opts) {
-    if (err) {
-      callback(err)
-    } else {
-      run('git add ' + opts.filename, function (err, stdout, stderr) {
-        if (err || stderr) {
-          callback(err || new Error(stderr))
+    fs.writeFile(path.join(testFolder, opts.filename), opts.content, function (err) {
+        if (err) {
+            callback(err)
         } else {
-          callback(null, opts)
+            callback(null, opts)
         }
-      })
-    }
-  })
+    })
 }
 
-addFiles = function (number, callback) {
-  var files = []
-
-  var runner = function (err, data) {
-    if (err) {
-      callback(err)
+function addFile (opts, callback) {
+    if (typeof opts == 'function') {
+        callback = opts
+        opts = {}
     }
-    else {
-      files.push(data)
-      if (files.length == number) {
-        callback(null, files)
-      }
-      else {
-        addFile(runner)
-      }
-    }
-  }
 
-  addFile(runner)
-}
-
-addAndCommitFile = function (opts, callback) {
-
-  if (typeof opts == 'function') {
-    callback = opts
-    opts = {}
-  }
-
-  addFile(opts, function (err, opts) {
-    if (err) {
-      callback(err)
-    } else {
-      run('git commit -m \'adding ' + opts.filename + '\'', function (err, stdout, stderr) {
-        if (err || stderr) {
-          callback(err || new Error(stderr))
+    newFile(opts, function (err, opts) {
+        if (err) {
+            callback(err)
         } else {
-          callback(null, opts)
+            run('git add ' + opts.filename, function (err, stdout, stderr) {
+                if (err || stderr) {
+                    callback(err || new Error(stderr))
+                } else {
+                    callback(null, opts)
+                }
+            })
         }
+    })
+}
 
-      })
+function addFiles (number, callback) {
+    var files = []
+
+    var runner = function (err, data) {
+        if (err) {
+            callback(err)
+        }
+        else {
+            files.push(data)
+            if (files.length == number) {
+                callback(null, files)
+            }
+            else {
+                addFile(runner)
+            }
+        }
     }
-  })
+
+    addFile(runner)
+}
+
+function addAndCommitFile (opts, callback) {
+
+    if (typeof opts == 'function') {
+        callback = opts
+        opts = {}
+    }
+
+    addFile(opts, function (err, opts) {
+        if (err) {
+            callback(err)
+        } else {
+            run('git commit -m \'adding ' + opts.filename + '\'', function (err, stdout, stderr) {
+                if (err || stderr) {
+                    callback(err || new Error(stderr))
+                } else {
+                    callback(null, opts)
+                }
+
+            })
+        }
+    })
 }
