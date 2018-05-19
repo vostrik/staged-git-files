9bf337eb1e25dd0c5d80368dd1bdfc30ac792a57
vostrik <mail@vostrik.com>
2018-05-19 23:30:36 +0300

    Generate tests for codeToStatus map

--- test/helpers/code-to-status.test.js
+++ test/helpers/code-to-status.test.js
@@ -1,39 +1,23 @@
-describe('Code to status handler', () => {
-  const statusMap = require('../../src/helpers/code-to-status')
+const codeToStatus = require('../../src/helpers/code-to-status')
 
-  test('Code \'A\' should return status \'Added\'', () => {
-    expect(statusMap('A')).toBe('Added')
-  })
+describe('codeToStatus() handler', () => {
+  const map = {
+    'A': 'Added',
+    'C': 'Copied',
+    'D': 'Deleted',
+    'M': 'Modified',
+    'R': 'Renamed',
+    'T': 'Type-Change',
+    'U': 'Unmerged',
+    'X': 'Unknown',
+    'B': 'Broken'
+  }
 
-  test('Code \'C\' should return status \'Copied\'', () => {
-    expect(statusMap('C')).toBe('Copied')
-  })
-
-  test('Code \'D\' should return status \'Deleted\'', () => {
-    expect(statusMap('D')).toBe('Deleted')
-  })
-
-  test('Code \'M\' should return status \'Modified\'', () => {
-    expect(statusMap('M')).toBe('Modified')
-  })
-
-  test('Code \'R\' should return status \'Renamed\'', () => {
-    expect(statusMap('R')).toBe('Renamed')
-  })
-
-  test('Code \'T\' should return status \'Type-Change\'', () => {
-    expect(statusMap('T')).toBe('Type-Change')
-  })
-
-  test('Code \'U\' should return status \'Unmerged\'', () => {
-    expect(statusMap('U')).toBe('Unmerged')
-  })
-
-  test('Code \'X\' should return status \'Unknown\'', () => {
-    expect(statusMap('X')).toBe('Unknown')
-  })
-
-  test('Code \'B\' should return status \'Broken\'', () => {
-    expect(statusMap('B')).toBe('Broken')
-  })
+  Object
+    .entries(map)
+    .forEach(([code, status]) => {
+      it(`returns status '${status}' from code '${code}'`, () => {
+        expect(codeToStatus(code)).toBe(status)
+      })
+    })
 })
