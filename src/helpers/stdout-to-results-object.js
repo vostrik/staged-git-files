5177613c1b83beb18061cabcf8936179caaede28
vostrik <mail@vostrik.com>
2018-05-20 00:18:24 +0300

    Add initial tests for stdout to results

--- src/helpers/stdout-to-results-object.js
+++ src/helpers/stdout-to-results-object.js
@@ -2,12 +2,13 @@ const fs = require('fs')
 
 const codeToStatus = require('./code-to-status')
 
-module.exports = function stdoutToResultsObject (stdout, includeContent, cwd) {
+module.exports = function stdoutToResultsObject (stdout, includeContent, cwd = '') {
   const results = stdout
     .split('\n')
     .reduce((results, line) => {
       if (line === '') return results
       const parts = line.split('\t')
+      if (parts.length < 2) return results
       const result = {
         filename: parts[2] || parts[1],
         status: codeToStatus(parts[0])
--- test/helpers/stdout-to-results-object.test.js
+++ test/helpers/stdout-to-results-object.test.js
@@ -1,1 +1,39 @@
-describe('Std-to-results handler', () => { })
+const stdoutToResultsObject = require('../../src/helpers/stdout-to-results-object')
+
+describe('stdoutToResultsObject() handler', () => {
+  it('returns list of file statuses', () => {
+    const stdout = 'A\tindex.js\nM\ttest.js'
+    const results = [
+      {
+        filename: 'index.js',
+        status: 'Added'
+      },
+      {
+        filename: 'test.js',
+        status: 'Modified'
+      }
+    ]
+    expect(stdoutToResultsObject(stdout)).toEqual(results)
+  })
+
+  it('returns list of file statuses with content on flag includeContent=true', () => {
+    
+  })
+
+  it('returns list of file statuses with content error on missed files', () => {
+    const stdout = 'A\tindex.js\nM\ttest.js'
+    const results = [
+      {
+        err: new Error('ENOENT: no such file or directory, open \'/index.js\''),
+        filename: 'index.js',
+        status: 'Added'
+      },
+      {
+        err: new Error('ENOENT: no such file or directory, open \'/test.js\''),
+        filename: 'test.js',
+        status: 'Modified'
+      }
+    ]
+    expect(stdoutToResultsObject(stdout, true)).toEqual(results)
+  })
+})
