3af7f2f87da3c1a31b36eda5c20b47911a01230f
Roman Shumskiy <rshumskiy@gmail.com>
2018-05-19 14:25:00 +0300

    Command runner test

--- test/helpers/run.test.js
+++ test/helpers/run.test.js
@@ -1,1 +1,27 @@
-describe('Run handler', () => { })
+describe('Run handler', () => {
+  test('Should read node version', () => {
+    const run = require('../../src/helpers/run')
+
+    run('node -v', false, (err, stdout, stderr) => {
+      expect(err).toBeNull()
+
+      expect(typeof stdout).toBe('string')
+
+      expect(stdout.trim()).toBe(process.version)
+
+      expect(stderr).toBeFalsy()
+    })
+  })
+
+  test('Should process debug flag', () => {
+    global.console = {
+      log: jest.fn()
+    }
+
+    const run = require('../../src/helpers/run')
+
+    run('node -v', true, () => {
+      expect(global.console.log).toHaveBeenCalledWith('RUNNING: node -v')
+    })
+  })
+})
